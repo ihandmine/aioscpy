ebb2dce1714c384fb36b3130d601757c42f06867
ihandmine <handmine@outlook.com>
2022-04-01 14:24:58 +0800

    ad moudle

--- aioscpy/core/scheduler/memory.py
+++ aioscpy/core/scheduler/memory.py
@@ -1,7 +1,7 @@
 from aioscpy.utils.misc import load_object, create_instance
 
 
-class Scheduler(object):
+class MemoryScheduler(object):
 
     def __init__(self):
         pass
@@ -10,18 +10,3 @@ class Scheduler(object):
     def from_crawler(cls, crawler):
         setting = crawler.setting
         _queue_cls = load_object(setting.get('SCHEDULER_PRIORITY_QUEUE'))
-        _key = setting.get('SCHEDULER_QUEUE_KEY')
-        _
-        _queue = create_instance(_queue_cls, )
-
-
-    def enqueue_request(self, request):
-        self.queue.push(request)
-        return True
-
-    def next_request(self):
-        request = self.queue.pop()
-        return request
-
-    def has_pending_requests(self):
-        return len(self) > 0
