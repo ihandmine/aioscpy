a4dac05ce8bf76b2293475e6c2e8f568b27a1673
ihandmine <handmine@outlook.com>
2022-04-01 15:00:22 +0800

    commit

--- aioscpy/core/scheduler/redis.py
+++ aioscpy/core/scheduler/redis.py
@@ -2,11 +2,13 @@ from aioscpy.utils.misc import load_object, create_instance
 from aioscpy.core.scheduler import Scheduler
 
 
-class MemoryScheduler(Scheduler):
+class RedisScheduler(Scheduler):
 
     @classmethod
     def from_crawler(cls, crawler):
         setting = crawler.setting
         _queue_cls = load_object(setting.get('SCHEDULER_PRIORITY_QUEUE'))
-        _queue = create_instance(_queue_cls)
+        _queue_key = setting.get('SCHEDULER_QUEUE_KEY')
+        _queue_tcp = setting.get('SCHEDULER_QUEUE_TCP')
+        _queue = create_instance(_queue_cls, _queue_key, _queue_tcp)
         return cls(_queue)
