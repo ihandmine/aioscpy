0e3b7239fae54a6dd59de2f1b17e98bff7f282ea
ihandmine <handmine@outlook.com>
2022-04-18 18:44:19 +0800

    feat: add loguru instead of logging moudle

--- aioscpy/utils/log.py
+++ aioscpy/utils/log.py
@@ -1,165 +1,107 @@
-# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
-import logging
 import sys
+import socket
 import warnings
-from logging.config import dictConfig
-
-# from twisted.python import log as twisted_log
-# from twisted.python.failure import Failure
-#
 import aioscpy
+
+from loguru import logger
+
 from aioscpy.exceptions import ScrapyDeprecationWarning
 from aioscpy.settings import Settings
 
-
-logger = logging.getLogger(__name__)
-
-
-# def failure_to_exc_info(failure):
-#     """Extract exc_info from Failure instances"""
-#     if isinstance(failure, Failure):
-#         return (failure.type, failure.value, failure.getTracebackObject())
+settings = Settings()
 
 
-class TopLevelFormatter(logging.Filter):
-    """Keep only top level loggers's name (direct children from root) from
-    records.
-
-    This filter will replace Scrapy loggers' names with 'scrapy'. This mimics
-    the old Scrapy log behaviour and helps shortening long names.
-
-    Since it can't be set for just one logger (it won't propagate for its
-    children), it's going to be set in the root handler, with a parametrized
-    ``loggers`` list where it should act.
-    """
-
-    def __init__(self, loggers=None):
-        self.loggers = loggers or []
-
-    def filter(self, record):
-        if any(record.name.startswith(l + '.') for l in self.loggers):
-            record.name = record.name.split('.', 1)[0]
-        return True
-
-
-DEFAULT_LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'loggers': {
-        'scrapy': {
-            'level': 'DEBUG',
+def set_log_config(formatter: str):
+    _log_config = {
+        "default": {
+            "handlers": [
+                {
+                    "sink": sys.stdout,
+                    "format": formatter,
+                    "level": "TRACE"
+                }
+            ],
+            "extra": {
+                "host": socket.gethostbyname(socket.gethostname()),
+                'log_name': 'default',
+                'type': 'None'
+            },
+            "levels": [
+                dict(name="TRACE", icon="✏️", color="<cyan><bold>"),
+                dict(name="DEBUG", icon="❄️", color="<blue><bold>"),
+                dict(name="INFO", icon="♻️", color="<bold>"),
+                dict(name="SUCCESS", icon="✔️", color="<green><bold>"),
+                dict(name="WARNING", icon="⚠️", color="<yellow><bold>"),
+                dict(name="ERROR", icon="❌️", color="<red><bold>"),
+                dict(name="CRITICAL", icon="☠️", color="<RED><bold>"),
+            ]
         },
-        'twisted': {
-            'level': 'ERROR',
-        },
+        'kafka': True
     }
-}
+    if settings.get('LOG_FILE', False):
+        _log_config['default']['handlers'].append({
+            "sink": f"{settings.get('LOG_FILENAME', __file__)}.log",
+            "format": formatter,
+            "level": "INFO",
+            "rotation": '1 week',
+            "retention": '30 days',
+            'encoding': 'utf-8'
+        })
+    return _log_config
 
 
-def configure_logging(settings=None, install_root_handler=True):
-    if not sys.warnoptions:
-        # Route warnings through python logging
-        logging.captureWarnings(True)
+class LogFormatter(object):
+    simple_formatter = '<green>{time:YYYY-MM-DD HH:mm:ss}</green> ' \
+                       '[<cyan>{name}</cyan>] ' \
+                       '<level>{level.icon}{level}</level>: ' \
+                       '<level>{message}</level> '
 
-    # observer = twisted_log.PythonLoggingObserver('twisted')
-    # observer.start()
+    default_formatter = '<green>{time:YYYY-MM-DD HH:mm:ss,SSS}</green> | ' \
+                        '[<cyan>{extra[log_name]}</cyan>] <cyan>{module}</cyan>:<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | ' \
+                        '<red>{extra[host]}</red> | ' \
+                        '<level>{level.icon}{level: <5}</level> | ' \
+                        '<level>{level.no}</level> | ' \
+                        '<level>{extra[type]}</level> | ' \
+                        '<level>{message}</level> '
 
-    dictConfig(DEFAULT_LOGGING)
+    kafka_formatter = '{time:YYYY-MM-DD HH:mm:ss,SSS}| ' \
+                      '[{extra[log_name]}] {module}:{name}:{function}:{line} | ' \
+                      '{extra[host]} | ' \
+                      '{process} | ' \
+                      '{thread} | ' \
+                      '{level: <5} | ' \
+                      '{level.no} | ' \
+                      '{extra[type]}| ' \
+                      '{message} '
 
-    if isinstance(settings, dict) or settings is None:
-        settings = Settings(settings)
+    @classmethod
+    def setter_log_handler(cls, log, callback=None):
+        assert callable(callback), 'callback must be a callable object'
+        log.add(callback, format=cls.kafka_formatter)
 
-    if settings.getbool('LOG_STDOUT'):
-        sys.stdout = StreamLogger(logging.getLogger('stdout'))
+    @classmethod
+    def get_logger(cls, log, name=None):
+        log_config = set_log_config(cls.simple_formatter)
+        config = log_config.pop('default', {})
+        if name:
+            config['extra']['log_name'] = name
+        log.configure(**config)
+        return log
 
-    if install_root_handler:
-        install_scrapy_root_handler(settings)
-
-
-def install_scrapy_root_handler(settings):
-    global _scrapy_root_handler
-
-    if (_scrapy_root_handler is not None
-            and _scrapy_root_handler in logging.root.handlers):
-        logging.root.removeHandler(_scrapy_root_handler)
-    logging.root.setLevel(logging.NOTSET)
-    _scrapy_root_handler = _get_handler(settings)
-    logging.root.addHandler(_scrapy_root_handler)
-
-
-def get_scrapy_root_handler():
-    return _scrapy_root_handler
-
-
-_scrapy_root_handler = None
-
-
-def _get_handler(settings):
-    """ Return a log handler object according to settings """
-    filename = settings.get('LOG_FILE')
-    if filename:
-        encoding = settings.get('LOG_ENCODING')
-        handler = logging.FileHandler(filename, encoding=encoding)
-    elif settings.getbool('LOG_ENABLED'):
-        handler = logging.StreamHandler()
-    else:
-        handler = logging.NullHandler()
-
-    formatter = logging.Formatter(
-        fmt=settings.get('LOG_FORMAT'),
-        datefmt=settings.get('LOG_DATEFORMAT')
-    )
-    handler.setFormatter(formatter)
-    handler.setLevel(settings.get('LOG_LEVEL', "INFO"))
-    if settings.getbool('LOG_SHORT_NAMES'):
-        handler.addFilter(TopLevelFormatter(['scrapy']))
-    return handler
-
-
-def log_scrapy_info(settings):
-    logger.info("Scrapy %(version)s started (bot: %(bot)s)",
-                {'version': aioscpy.__version__, 'bot': settings['BOT_NAME']})
-
-
-class StreamLogger(object):
-    """Fake file-like stream object that redirects writes to a logger instance
-
-    Taken from:
-        https://www.electricmonk.nl/log/2011/08/14/redirect-stdout-and-stderr-to-a-logger-in-python/
-    """
-    def __init__(self, logger, log_level=logging.INFO):
-        self.logger = logger
-        self.log_level = log_level
-        self.linebuf = ''
-
-    def write(self, buf):
-        for line in buf.rstrip().splitlines():
-            self.logger.log(self.log_level, line.rstrip())
-
-    def flush(self):
-        for h in self.logger.handlers:
-            h.flush()
-
-
-class LogCounterHandler(logging.Handler):
-    """Record log levels count into a crawler stats"""
-
-    def __init__(self, crawler, *args, **kwargs):
-        super(LogCounterHandler, self).__init__(*args, **kwargs)
-        self.crawler = crawler
-
-    def emit(self, record):
-        sname = 'log_count/{}'.format(record.levelname)
-        # self.crawler.stats.inc_value(sname)
+    @staticmethod
+    def format(spider, meta):
+        if hasattr(spider, 'logging_keys'):
+            logging_txt = []
+            for key in spider.logging_keys:
+                if meta.get(key, None) is not None:
+                    logging_txt.append(u'{0}:{1} '.format(key, meta[key]))
+            logging_txt.append('successfully')
+            return ' '.join(logging_txt)
 
 
 def logformatter_adapter(logkws):
-    """
-    Helper that takes the dictionary output from the methods in LogFormatter
-    and adapts it into a tuple of positional arguments for logger.log calls,
-    handling backward compatibility as well.
-    """
     if not {'level', 'msg', 'args'} <= set(logkws):
         warnings.warn('Missing keys in LogFormatter method',
                       ScrapyDeprecationWarning)
@@ -169,10 +111,20 @@ def logformatter_adapter(logkws):
                       'deprecated, use `msg` instead',
                       ScrapyDeprecationWarning)
 
-    level = logkws.get('level', logging.INFO)
+    level = logkws.get('level', 'INFO')
     message = logkws.get('format', logkws.get('msg'))
-    # NOTE: This also handles 'args' being an empty dict, that case doesn't
-    # play well in logger.log calls
     args = logkws if not logkws.get('args') else logkws['args']
 
-    return (level, message, args)
+    return level, message, args
+
+
+def std_log_aioscpy_info(settings):
+    logger.info("aioscpy %(version)s started (bot: %(bot)s)",
+                {'version': aioscpy.__version__, 'bot': settings['BOT_NAME']})
+
+
+lof = LogFormatter
+
+logger = lof.get_logger(logger)
+
+
