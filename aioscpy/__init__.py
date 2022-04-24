1c8f6bb4ec37c586238831af9b827de8f4f538c8
ihandmine <handmine@outlook.com>
2022-04-24 18:40:17 +0800

    feat: add metacls object_ref method

--- aioscpy/__init__.py
+++ aioscpy/__init__.py
@@ -1,2 +1,13 @@
+from aioscpy.inject import CSlot, DependencyInjection
+
+
+class object_ref(type):
+    def __init__(msc, *args, **kwargs):
+        msc.ref = CSlot()
+        if msc.ref.empty():
+            DependencyInjection().inject()
+        msc.logger = msc.ref.get("log").logger
+        super().__init__(*args, **kwargs)
+
 
 __version__ = "0.0.1"
