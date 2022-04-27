670da17936b22934a6b8f56f9b0cde63186908bd
ihandmine <handmine@outlook.com>
2022-04-27 17:59:49 +0800

    feat: update redis new version pipeline exec method

--- aioscpy/queue/redis/_queue_async.py
+++ aioscpy/queue/redis/_queue_async.py
@@ -1,25 +1,29 @@
-import aioredis
-
-from aioredis import Redis
+from aioredis import Redis, BlockingConnectionPool
 
 from aioscpy.queue import BaseQueue
 
 
 class PriorityQueue(BaseQueue):
+    def __init__(self, server, spider, key=None, serializer="pickle"):
+        super().__init__(server, spider, key)
+        self.serializer = self.__compat__[serializer]
+
     async def qsize(self) -> int:
         return await self.server.zcard(self.key)
 
     async def push(self, request: dict):
         data = self._encode_request(request)
         score = -request.get('priority', 1)
-        await self.server.zadd(self.key, score, data)
+        await self.server.zadd(self.key, {data: score})
 
     async def pop(self, timeout: int = 0) -> dict:
-        pipe = self.server.multi_exec()
-        pipe.zrange(self.key, 0, 0)
-        pipe.zremrangebyrank(self.key, 0, 0)
-        results, count = await pipe.execute()
-        if results: 
+        async with self.server.pipeline(transaction=True) as pipe:
+            results, count = await (
+                pipe.zrange(self.key, 0, 0)
+                    .zremrangebyrank(self.key, 0, 0)
+                    .execute()
+            )
+        if results:
             return self._decode_request(results[0])
 
 
@@ -28,7 +32,9 @@ class AsyncRedis:
 
     def __init__(self, *args, **kwargs):
         self.args = args
-        self.kwargs = self.validator(kwargs)
+        if not kwargs:
+            self.kwargs = self.validator(kwargs)
+        self.kwargs = kwargs
 
     @staticmethod
     def validator(params: dict) -> dict:
@@ -36,24 +42,29 @@ class AsyncRedis:
         params.setdefault('port', 6379)
         params.setdefault('db', 1)
         params.setdefault('password', 'admin')
-        params['address'] = (params.pop('host'), params.pop('port'))
         return params
 
     @property
     async def get_redis_pool(self) -> Redis:
         if not self.__redis_instance:
-            self.__redis_instance = await aioredis.create_redis_pool(*self.args, **self.kwargs)
+            url = self.kwargs.pop('url', None)
+            if url:
+                connection_pool = BlockingConnectionPool.from_url(url, **self.kwargs)
+            else:
+                connection_pool = BlockingConnectionPool(**self.kwargs)
+            self.__redis_instance = Redis(connection_pool=connection_pool)
         return self.__redis_instance
 
     async def close(self):
         if self.__redis_instance:
-            self.__redis_instance.close()
-            await self.__redis_instance.wait_closed()
+            await self.__redis_instance.close()
 
 
-async def aio_priority_queue(key: str, redis_tcp: dict) -> PriorityQueue:
+async def aio_priority_queue(key: str, redis_tcp, spider) -> PriorityQueue:
+    if isinstance(redis_tcp, str):
+        redis_tcp = {'url': redis_tcp}
     server = await AsyncRedis(**redis_tcp).get_redis_pool
-    return PriorityQueue(server=server, key=key)
+    return PriorityQueue(server=server, spider=spider, key=key, serializer='json')
 
 
 spider_aio_priority_queue = aio_priority_queue
@@ -61,7 +72,13 @@ spider_aio_priority_queue = aio_priority_queue
 """
 # unit test example
 async def run():
-    queue = await redis_client('message:queue')
+    REDIS_TCP = {
+                "host": "172.16.7.172",
+                "port": 6379,
+                "password": "123456",
+                "db": 15
+            }
+    queue = await aio_priority_queue('message:queue', REDIS_TCP)
     # await queue.push({"url": "https://www.baidu.com/?kw=1", "task_id": '123'})
     print(await queue.pop())
 
@@ -69,6 +86,5 @@ async def run():
 if __name__ == "__main__":
     import asyncio
     asyncio.run(run())
-
 """
 
