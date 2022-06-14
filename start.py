d216c2eda65047c4f2800a64f9bafeadf8c704fa
ihandmine <handmine@outlook.com>
2022-06-14 17:33:56 +0800

    refactor: start.py method to run

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 from aioscpy.crawler import call_grace_instance
+from aioscpy.utils.tools import get_project_settings
 
 """start spider method one:
 from cegex.baidu import BaiduSpider
@@ -11,7 +12,17 @@ process.start()
 """
 
 
-process = call_grace_instance("crawler_process")
-process.load_spider('./cegex')
-process.start()
+def load_file_to_execute():
+    process = call_grace_instance("crawler_process", get_project_settings())
+    process.load_spider(path='./spiders')
+    process.start()
 
+
+def load_name_to_execute():
+    process = call_grace_instance("crawler_process", get_project_settings())
+    process.crawl('baidu', path="./cegex")
+    process.start()
+
+
+if __name__ == '__main__':
+    load_name_to_execute()
