1ab45f2751b2f40257ac95ff02e7ddcc2931b3df
ihandmine <handmine@outlook.com>
2022-11-25 17:35:54 +0800

    feat: add json_request usage

--- cegex/httpbin_post.py
+++ cegex/httpbin_post.py
@@ -11,9 +11,29 @@ class HttpBinPostSpider(Spider):
     start_urls = ['http://httpbin.org/post' for _ in range(20)]
 
     async def start_requests(self):
+        """
+        : request usage description:
+        <form_request>: data = body
+            [header]: Content-Type: application/x-www-form-urlencoded
+            [method]: GET or POST
+            [body]: a=1&b=1 or 
+                  {
+                    'a': 1,
+                    'b': 2
+                  }
+        # supported special scenarios about json request
+        <json_request>: json = body
+            [header]: Content-Type: application/json
+            [method]: POST
+            [body]: {
+                'a': 1,
+                'b': 2
+            }
+        """
         for url in self.start_urls:
             yield call_grace_instance(
-                self.di.get('form_request'), 
+                # self.di.get('form_request'), 
+                self.di.get('json_request'), 
                 url, 
                 method='POST',
                 formdata={"b": '11'}
