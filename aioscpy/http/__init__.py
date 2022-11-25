1446be5e9482d118e662cba2cf7d45b04ef2aace
ihandmine <handmine@outlook.com>
2022-11-25 17:33:38 +0800

    feat: _set_body method remove to_bytes

--- aioscpy/http/__init__.py
+++ aioscpy/http/__init__.py
@@ -1,5 +1,6 @@
 from aioscpy.http.request import Request
 from aioscpy.http.request.form import FormRequest
+from aioscpy.http.request.json import JsonRequest
 
 from aioscpy.http.response import Response
 from aioscpy.http.response.text import TextResponse
@@ -8,6 +9,7 @@ from aioscpy.http.response.text import TextResponse
 __all__ = [
     Request,
     FormRequest,
+    JsonRequest,
     Response,
     TextResponse
 ]
