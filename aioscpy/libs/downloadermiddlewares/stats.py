192ed347e38ebd60a3c16e53fe6339259bc2d63a
ihandmine <handmine@outlook.com>
2023-10-20 15:58:16 +0800

    fix: request.body format to use

--- aioscpy/libs/downloadermiddlewares/stats.py
+++ aioscpy/libs/downloadermiddlewares/stats.py
@@ -22,7 +22,7 @@ def request_httprepr(request: "Request") -> bytes:
     if request.headers:
         s += request.headers.to_string() + b"\r\n"
     s += b"\r\n"
-    s += str(request.body).encode() if isinstance(request.body, dict) else request.body
+    s += str(request.body).encode() if request.body and isinstance(request.body, dict) else b""
     return s
 
 
