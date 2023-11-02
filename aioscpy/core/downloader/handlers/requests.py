4d073369ae3bb7f07b0439830c248f037cd30263
ihandmine <handmine@outlook.com>
2023-11-02 15:38:04 +0800

    fix: requests handler tls && proxy

--- aioscpy/core/downloader/handlers/requests.py
+++ aioscpy/core/downloader/handlers/requests.py
@@ -2,6 +2,7 @@ import asyncio
 import requests
 
 from anti_header import Headers
+from anti_useragent.utils.cipers import generate_cipher
 
 
 class RequestsDownloadHandler(object):
@@ -22,15 +23,6 @@ class RequestsDownloadHandler(object):
         headers = request.headers
         if isinstance(headers, Headers):
             headers = headers.to_unicode_dict()
-        requests_client_session = {}
-
-        if request.meta.get("proxy"):
-            requests_client_session['proxies'] = {
-                'http': request.meta["proxy"],
-                'https': request.meta["proxy"]
-            }
-            self.logger.debug(f"use {request.meta['proxy']} crawling: {request.url}")
-
         requests_client_session = {
             'timeout': self.settings.get('DOWNLOAD_TIMEOUT'),
             'cookies': dict(request.cookies),
@@ -39,6 +31,22 @@ class RequestsDownloadHandler(object):
             "data": request.body,
             "json": request.json,
         }
+
+        if request.meta.get('TLS_CIPHERS') or self.settings.get('TLS_CIPHERS'):
+            requests.adapters.DEFAULT_RETRIES = 10
+            requests.packages.urllib3.disable_warnings()
+            cipers_real = generate_cipher()
+            self.logger.debug(cipers_real)
+            requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = cipers_real
+
+        if request.meta.get("proxy"):
+            requests_client_session['proxies'] = {
+                'http': request.meta["proxy"],
+                'https': request.meta["proxy"]
+            }
+            self.logger.debug(f"use {request.meta['proxy']} crawling: {request.url}")
+
+
         
         response = await asyncio.to_thread(requests.request, request.method, request.url, **requests_client_session)
 
